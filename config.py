9ba1f4ea17924a5a676ea7af45558a1d4e453144
Jess <jmerriman95@live.com>
2021-10-10 22:33:01 +1100

    error handling for config file

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 import json
-conf_file = open("config.json", "r")
-config_settings = json.loads(conf_file.read())
-DISCORD_BOT_TOKEN = config_settings["DISCORD_BOT_TOKEN"]
-FFMPEG_PATH       = config_settings["FFMPEG_PATH"]
-COMMAND_PREFIX    = config_settings["COMMAND_PREFIX"]
-LISTENING_TO      = "{}help".format(COMMAND_PREFIX)
+import colorama
+from colorama import Fore as fg
+from colorama import Style as st
+colorama.init()
+
+try:
+    conf_file = open("config.json", "r")
+    config_settings = json.loads(conf_file.read())
+    DISCORD_BOT_TOKEN = config_settings["DISCORD_BOT_TOKEN"]
+    FFMPEG_PATH       = config_settings["FFMPEG_PATH"]
+    COMMAND_PREFIX    = config_settings["COMMAND_PREFIX"]
+    LISTENING_TO      = "{}help".format(COMMAND_PREFIX)
+except json.decoder.JSONDecodeError as e:
+    print(f"{fg.RED}FATAL ERROR:{st.RESET_ALL} config.json looks to be incorrectly formatted: {e.msg}")
+    print("Correct any issues and try running JukeBot again.")
+    exit()
