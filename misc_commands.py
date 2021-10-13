51f1ea539c2ac00c379a84ccbb26f3e387c500c3
Jess <jmerriman95@live.com>
2021-10-14 10:16:11 +1100

    fixed update msg

--- misc_commands.py
+++ misc_commands.py
@@ -131,13 +131,14 @@ class Other(commands.Cog):
     @commands.command()
     @is_developer()
     async def update(self, ctx):
+        await ctx.message.delete()
         reply = dialogBox("Version", "JukeBot has been updated!",
         """**Thank you for helping test out JukeBot while I still work on it!**\n
         **I've fixed a few bugs, added some new commands (`!nowplaying`, `!clear`), and have made the experience hopefully work a lot smoother for y'all.**
 
         _(Please keep in mind that JukeBot is still a work-in-progress! I guess you could say it's \"in alpha\". If you're currently lucky enough to have JukeBot running in your server, expect there to be some hiccups and bugs - report them to me on Discord at squig#1312, or via the ticket system at https://github.com/squigjess/JukeBot/issues if you see any!)_""")
         reply.set_image(url="https://media.discordapp.net/attachments/887723918574645331/895242544223518740/discordjp.jpg")
-        await ctx.send(embed=reply)
+        await ctx.send(embed=reply, delete_after=86400)
 
 # This line must be left intact, otherwise the build will fail.
 # lmfao
