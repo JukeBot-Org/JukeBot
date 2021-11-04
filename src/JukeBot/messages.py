c4f9d8fa69c27c15391f9033e5284faddafd6f04
Jess <jmerriman95@live.com>
2021-11-05 01:46:46 +1100

    Move long strings to constants / .txt file Fixes #32

---
+++ src/JukeBot/messages.py
@@ -0,0 +1,35 @@
+from JukeBot import config
+
+EPHEMERAL_FOOTER = "This message will automatically disappear shortly"
+
+CANNOT_PLAY = "**Possible reasons:**\n- Incorrect video format\n- No results for search query\n- Streaming disabled by YouTube uploader\n- Malformed or invalid link provided."
+
+PRETTY_QUEUE_HEADER = "#  Track title                                            Duration "
+
+PLS_RESUME = f"Type `{config.COMMAND_PREFIX}resume` to resume the track."
+
+UPDATE_FOOTER = "This update message will automatically disappear after 24 hrs.\n\nhttp://squigjess.github.io/JukeBot"
+
+BANNER_IMG = "https://media.discordapp.net/attachments/887723918574645331/895242544223518740/discordjp.jpg"
+THUMB_IMG = "https://cdn.discordapp.com/avatars/886200359054344193/4da9c1e1257116f08c99c904373b47b7.png"
+
+# =============================== LENGTHY BOIS ============================== #
+
+ABOUT = """**JukeBot** is a self-hostable audio streaming bot that runs on spite, a love for freedom, and Python 3.\n
+You can find more information on the project, as well as download the program to host your own instance of JukeBot, at **https://squigjess.github.io/JukeBot**
+
+Please keep in mind that JukeBot is still a work-in-progress! I guess you could say it's \"in alpha\". If you're currently lucky enough to have JukeBot running in your server, expect there to be some hiccups and bugs - report them to https://github.com/squigjess/JukeBot/issues if you see any!"""
+
+UPDATE_CHANGELOG = """**Thank you for helping test out JukeBot while I still work on it!**\n
+**This is quite a milestone update; a lot of changes have been implemented, bringing JukeBot closer to being a fully-stable audio streaming bot!**
+    - A bunch of major bugs have been fixed.
+    - New commands are here - `!pause`, `!resume`, `!nowplaying`, and `!stop`/`!disconnect`.
+    - You can now remove individual tracks from the queue with `!clear x`, where `x` is the number of the track in the queue (see `!queue`).
+    - Heaps of bugs fixed.
+    - If you have multiple Discord servers in which you would like to run your copy of JukeBot, that's now possible! I mean, it was _always_ possible, it just didnt't work. At all.
+    - The auto-disconnect feature is back after being temporarily disabled for a minute.
+    - No I mean there were a **lot** of bugs in the previous alpha version. Christ alive, it was bad.
+
+Currently, I'm working on the Spotify integration problem and the ability to save queues. Hold tight!
+
+_As you jam out, please keep in mind that JukeBot is still a work-in-progress. If you're currently lucky enough to have JukeBot running in your server, expect there to be some hiccups and bugs - please report them to me on Discord at squig#1312, or via the ticket system at https://github.com/squigjess/JukeBot/issues if you see any._"""
--- src/JukeBot/queue.py
+++ src/JukeBot/queue.py
@@ -1,15 +1,31 @@
+import JukeBot
 from JukeBot.utils import trim
 import nextcord
 
 
 class Queue:
     def __init__(self, guild: nextcord.Guild):
-        self.tracks = []  # Stores an ordered list of the tracks in the queue. 0th is playing currently, 1st is next, and so on.
-        self.is_empty = lambda: not bool(self.tracks)  # lmfao
-        self.is_playing = False  # Tracks if the bot is playing. Need to remove this in favour of built-in nextcord equivalent.
-        self.audio_player = None  # A nextcord.VoiceChannel object representing the VC we're in currently.
-        self.guild = guild  # A nextcord.Guild object that contains info about the guild which this queue is for.
-        self.current_idle_time = 0  # This will increment for as long as the bot idles in a voice channel.
+        # Stores an ordered list of the tracks in the queue.
+        # 0th is playing currently, 1st is next, and so on.
+        self.tracks = []
+
+        # lmfao
+        self.is_empty = lambda: not bool(self.tracks)
+
+        # Tracks if the bot is playing. Need to remove this in favour of the
+        # built-in nextcord equivalent (nextcord.VoiceChannel.is_playing).
+        self.is_playing = False
+
+        # A nextcord.VoiceChannel object representing the VC we're
+        # in currently.
+        self.audio_player = None
+
+        # A nextcord.Guild object that contains info about the guild which
+        # this queue is for.
+        self.guild = guild
+
+        # This will increment for as long as the bot idles in a voice channel.
+        self.current_idle_time = 0
 
     def clear(self):
         self.tracks = []
@@ -17,8 +33,10 @@ class Queue:
     def remove_track(self, track_index_to_remove):
         self.tracks.pop(track_index_to_remove)
 
+    # TODO: Will eventually add up the total duration of all tracks
+    # in the queue.
     def total_time(self):
-        return None  # Will eventually add up the total duration of all tracks in the queue.
+        return None
 
     def add_track(self, track_data):
         self.tracks.append(track_data)
@@ -30,7 +48,7 @@ class Queue:
         if self.is_empty():
             return "Empty queue!"
         else:
-            header = "#  Track title                                            Duration "
+            header = JukeBot.messages.PRETTY_QUEUE_HEADER
             for track in range(0, len(self.tracks)):
                 queue_pos = track + 1
                 trimmed_title = trim(self.tracks[track].title)
